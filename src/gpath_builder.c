3f4608e67c2c84778ce45d1aa43b58ce01fb2ec1
Łukasz Zalewski <zalewszczak@gmail.com>
2014-12-18 20:27:01 -0800

    small improvements

--- src/gpath_builder.c
+++ src/gpath_builder.c
@@ -9,8 +9,7 @@ bool recursive_bezier_fixed(GPathBuilder *builder,
                             int32_t x1, int32_t y1,
                             int32_t x2, int32_t y2,
                             int32_t x3, int32_t y3,
-                            int32_t x4, int32_t y4){
-
+                            int32_t x4, int32_t y4) {
   // Calculate all the mid-points of the line segments
   int32_t x12   = (x1 + x2) / 2;
   int32_t y12   = (y1 + y2) / 2;
@@ -26,21 +25,25 @@ bool recursive_bezier_fixed(GPathBuilder *builder,
   int32_t y1234 = (y123 + y234) / 2;
 
   // Angle Condition
-  int32_t a23 = atan2_lookup((int16_t)((y3 - y2) / fixedpoint_base), (int16_t)((x3 - x2) / fixedpoint_base));
-  int32_t da1 = abs(a23 - atan2_lookup((int16_t)((y2 - y1) / fixedpoint_base), (int16_t)((x2 - x1) / fixedpoint_base)));
-  int32_t da2 = abs(atan2_lookup((int16_t)((y4 - y3) / fixedpoint_base), (int16_t)((x4 - x3) / fixedpoint_base)) - a23);
-  
+  int32_t a23 = atan2_lookup((int16_t)((y3 - y2) / fixedpoint_base),
+                             (int16_t)((x3 - x2) / fixedpoint_base));
+  int32_t da1 = abs(a23 - atan2_lookup((int16_t)((y2 - y1) / fixedpoint_base),
+                                       (int16_t)((x2 - x1) / fixedpoint_base)));
+  int32_t da2 = abs(atan2_lookup((int16_t)((y4 - y3) / fixedpoint_base),
+                                 (int16_t)((x4 - x3) / fixedpoint_base)) - a23);
+
   if (da1 >= TRIG_MAX_ANGLE) {
     da1 = TRIG_MAX_ANGLE - da1;
   }
-  
+
   if (da2 >= TRIG_MAX_ANGLE) {
     da2 = TRIG_MAX_ANGLE - da2;
   }
 
-  if (da1 + da2 < max_angle_tolerance){
+  if (da1 + da2 < max_angle_tolerance) {
     // Finally we can stop the recursion
-    return gpath_builder_line_to_point(builder, GPoint(x1234 / fixedpoint_base, y1234 / fixedpoint_base));
+    return gpath_builder_line_to_point(builder, GPoint(x1234 / fixedpoint_base,
+                                                       y1234 / fixedpoint_base));
   }
 
   // Continue subdivision if points are being added successfully
@@ -48,13 +51,12 @@ bool recursive_bezier_fixed(GPathBuilder *builder,
       && recursive_bezier_fixed(builder, x1234, y1234, x234, y234, x34, y34, x4, y4)) {
     return true;
   }
-  
+
   return false;
 }
 
-bool bezier_fixed(GPathBuilder *builder, GPoint p1, GPoint p2, GPoint p3, GPoint p4){
-
-  //translate points to fixedpoint realms
+bool bezier_fixed(GPathBuilder *builder, GPoint p1, GPoint p2, GPoint p3, GPoint p4) {
+  // Translate points to fixedpoint realms
   int32_t x1 = p1.x * fixedpoint_base;
   int32_t x2 = p2.x * fixedpoint_base;
   int32_t x3 = p3.x * fixedpoint_base;
@@ -64,7 +66,7 @@ bool bezier_fixed(GPathBuilder *builder, GPoint p1, GPoint p2, GPoint p3, GPoint
   int32_t y3 = p3.y * fixedpoint_base;
   int32_t y4 = p4.y * fixedpoint_base;
 
-  if(recursive_bezier_fixed(builder, x1, y1, x2, y2, x3, y3, x4, y4)){
+  if (recursive_bezier_fixed(builder, x1, y1, x2, y2, x3, y3, x4, y4)) {
     return gpath_builder_line_to_point(builder, p4);
   }
   return false;
@@ -84,12 +86,15 @@ void gpath_builder_destroy(GPathBuilder *builder) {
 
 GPath *gpath_builder_create_path(GPathBuilder *builder) {
   // TODO: make this using the official SDK setters
-  // TODO: handle builder.next_point_index <= 1
+  if (builder->next_point_index <= 1) {
+    return NULL;
+  }
 
   size_t num_points = builder->next_point_index;
 
   // handle case where last point == first point => remove last point
-  while (num_points > 1 && gpoint_equal(&builder->points[0], &builder->points[num_points])) {
+  while (num_points > 1
+          && gpoint_equal(&builder->points[0], &builder->points[num_points])) {
     num_points--;
   }
 
@@ -119,7 +124,8 @@ bool gpath_builder_line_to_point(GPathBuilder *builder, GPoint to_point) {
   return true;
 }
 
-bool gpath_builder_curve_to_point(GPathBuilder *builder, GPoint to_point, GPoint control_point_1, GPoint control_point_2) {
+bool gpath_builder_curve_to_point(GPathBuilder *builder, GPoint to_point,
+                                  GPoint control_point_1, GPoint control_point_2) {
   GPoint from_point = builder->points[builder->next_point_index-1];
   return bezier_fixed(builder, from_point, control_point_1, control_point_2, to_point);
 }
